0670cc8a4152a361b69d625081d3ac8c49cb6914
Fernando <fmti91@hotmail.com>
2018-10-22 13:43:36 -0300

    one to many relationship

--- app/Http/Controllers/OneToManyController.php
+++ app/Http/Controllers/OneToManyController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Country;
+use App\Models\State;
 
 class OneToManyController extends Controller
 {
     public function oneToMany()
     {
+        //$country = Country::where('name', 'Brasil')->get()->first();
+        $keySearch = 'a';
+        $countries = Country::where('name', 'LIKE', "%{$keySearch}%")->get();
+        //echo $country->name;
 
+        foreach($countries as $country){
+            echo $country->name;
+
+        //$states = $country->states()->where('initials', 'PR')->get();
+        $states = $country->states()->get();
+
+        foreach($states as $state){
+            echo "<br>{$state->initials} - {$state->name}";
+           }
+        echo '<hr>';
+        }
     }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Location;
+use App\Models\State;
 
 class Country extends Model
 {
     protected $fillable = ['name'];
 
+    //return one
     public function location()
     {
         return $this->hasOne(Location::class);
+    }
+
+    //return many states
+    public function states()
+    {
+        return $this->hasMany(State::class);
     }
 }
